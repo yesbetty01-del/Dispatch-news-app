f2d21523e7413751526a7b3e53da62ded87ace21
yesbetty01-del <yesbetty01@gmail.com>
2026-08-16 23:53:21 +0300

    filter saved articles by their category name

--- src/app/(tabs)/saved.js
+++ src/app/(tabs)/saved.js
@@ -11,8 +11,15 @@ import useBookmark from '../../store/useBookmark';
 const Saved = () => {
     const { colors, spacing } = useTheme();
     const styles = createStyles(colors);
-    const articleLength = 2;
     const { bookmarks, setBookmarks, removeBookmark, addBookmark, loadBookmarks } = useBookmark();
+    const articleLength = bookmarks.length;
+    const [selectedCategory, setSelectedCategory] = useState("All");
+    const cats = [... new Set(bookmarks.map((item) => item.categoryName))];
+    const allCategories = [{_id: "all", categoryName: "All"}, ...cats.map((cat) => ({_id: cat, categoryName: cat}))];
+    if(bookmarks.length === 0 && selectedCategory !== "All"){
+        setSelectedCategory("All")
+    }
+    const filteredBookmarks = selectedCategory === "All" ? bookmarks : bookmarks.filter((item) => item.categoryName == selectedCategory);
 
     useEffect(() => {
         loadBookmarks();
@@ -21,16 +28,16 @@ const Saved = () => {
     return (
         <SafeAreaView style={[styles.container, {paddingHorizontal: spacing.xl}]}>
             <Header header={'Saved'} />
-            <Chips />
+            <Chips categories={allCategories} setSelectedCategory={setSelectedCategory} selectedCategory={selectedCategory} />
             {
-                bookmarks.length === 0 && (
+                filteredBookmarks.length === 0 && (
                     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                         <Text style={{color: colors.inkSecondary}}> No bookmarks found </Text>
                     </View>
                 )
             }
             <FlatList
-                data={bookmarks}
+                data={filteredBookmarks}
                 keyExtractor={(item) => item._id}
                 renderItem={({item}) => {
                     return (
